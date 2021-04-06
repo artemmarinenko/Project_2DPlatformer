78fa40e39957d6687868425df410b25a72283408
artemmarinenko671 <artemmarinenko671@gmail.com>
2021-04-06 17:50:05 +0300

    Basic controll added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 2;
+
+    [SerializeField]
+    private float _jumpHeight = 4 ;
+
+
+    [SerializeField]
+    private Rigidbody2D _rBody;
+
+    private SpriteRenderer _renderer;
+
+    private Animator _animator;
+
+     void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _renderer = this.gameObject.GetComponent<SpriteRenderer>();
+        _rBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            _rBody.velocity = Vector2.up * _jumpHeight;
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        //_rBody.velocity = new Vector2(0, 5);
+
+
+        if (Input.GetKey(KeyCode.D)) {
+             _renderer.flipX = false;
+            _animator.SetFloat("Speed", _speed);
+            _rBody.position += Vector2.right * _speed * Time.fixedDeltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A)) {
+            _animator.SetFloat("Speed", _speed);
+            _renderer.flipX = true;
+            _rBody.position += Vector2.left * _speed * Time.fixedDeltaTime;
+        }
+        
+        else
+        {
+            _animator.SetFloat("Speed", 0);
+        }
+        
+    }
+}
