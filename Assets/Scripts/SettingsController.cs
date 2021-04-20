88959135f1079671b6e2c6401c5b2b7c8d574fc8
artemmarinenko671 <artemmarinenko671@gmail.com>
2021-04-20 03:20:05 +0300

    Added listeners to buttons and slider

---
+++ Assets/Scripts/SettingsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    private bool isMusicOn = false;
+    private bool isSoundOn = false;
+    private bool isFacebookOn = false;
+    [SerializeField] private Button _music;
+    [SerializeField] private Button _sound;
+    [SerializeField] private Button _facebook;
+
+    [SerializeField] private Slider _slider;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        AddButtonsListeners();
+        _slider.onValueChanged.AddListener((float rate) => Debug.Log("Volume rate " + rate));
+        
+    }
+    
+    
+
+    private void AddButtonsListeners() {
+
+        _music.onClick.AddListener(() => {
+            string status = isMusicOn ? "On" : "off";
+            _music.GetComponentInChildren<Text>().text= status;
+            isMusicOn = !isMusicOn;
+            Debug.Log("Music " + status);
+        });
+
+        _sound.onClick.AddListener(() => {
+            string status = isSoundOn ? "On" : "off";
+            _sound.GetComponentInChildren<Text>().text = status;
+            isSoundOn = !isSoundOn;
+            Debug.Log("Sound " + status);
+        });
+
+
+        _facebook.onClick.AddListener(() => {
+            string status = isFacebookOn ? "On" : " off";
+            _facebook.GetComponentInChildren<Text>().text = status;
+            isFacebookOn = !isFacebookOn;
+            Debug.Log("Facebook " + status);
+        });
+    }
+}
