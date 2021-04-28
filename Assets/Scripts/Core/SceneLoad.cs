2c9cb12fdcae32e221af216a6c432ed116bc29b8
artemmarinenko671 <artemmarinenko671@gmail.com>
2021-04-28 22:53:30 +0300

    Main Menu scene added

---
+++ Assets/Scripts/Core/SceneLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoad : MonoBehaviour
+{
+    [SerializeField] private Button _startButton;
+    [SerializeField] private Button _exitButton;
+    [SerializeField] private Animator _animator;
+
+    private void Awake()
+    {
+        
+        
+
+
+        _startButton.onClick.AddListener(() =>
+        {
+           
+            StartCoroutine(WaitAnimationThenStart(0.8f)) ;
+        });
+    }
+
+    IEnumerator WaitAnimationThenStart(float time)
+    {
+        _animator.SetBool("isStarted", true);
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+   
+}
